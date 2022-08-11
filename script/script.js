fcdb192ea280a185fa4c445f30a8580ad1a1e081
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2022-08-11 17:41:37 +0300

    добавить readme, исправить ошибки js

--- script/script.js
+++ script/script.js
@@ -6,9 +6,11 @@ let closePopup = document.querySelector('.form__button-close');
 
 let formInput = document.querySelector('.form__input');
  
-let nameInput = formInput.querySelector('.form__item_el_name');
+let nameInput = document.querySelector('.form__item_el_name');
 
-let jobInput = formInput.querySelector('.form__item_el_job');
+let jobInput = document.querySelector('.form__item_el_job');
+
+let userInfo = document.querySelector('profile__user-information')
 
 let userName = document.querySelector('.profile__user-name');
 
@@ -18,17 +20,32 @@ openPopup.addEventListener('click', function() {
     popup.classList.add('popup_opened');
 })
 
-closePopup.addEventListener('click', function() {
+function popupClose ()  {
     popup.classList.remove('popup_opened');
-})
+}
+
+popupClose();
+
+closePopup.addEventListener('click', popupClose);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     
-    nameInput.value = userName.textContent;
-    jobInput.value = userJob.textContent;
+    nameInput.value = userName;
+    jobInput.value = userJob;
+
+    userInfo.insertAdjacentElementHTML = `
+        <div class="profile__user-information">
+          <span class="profile__user-name">${nameInput.value}</span>
+          <span class="profile__user-subname">${jobInput.value}</span>
+        </div>
+    `;
+
+    userName.textContent = nameInput.textContent
+    userJob.textContent = jobInput.textContent
+
 }
 
-    formElement.addEventListener('submit', formSubmitHandler); 
+    formInput.addEventListener('submit', formSubmitHandler); 
 
 
