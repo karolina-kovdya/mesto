5f1af9667377c144902015463b9172643891795a
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2022-08-13 00:18:53 +0300

    исправить замечания ревьюера

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+let popup = document.querySelector('.popup');
+
+let openPopup = document.querySelector('.profile__button_type_edit');
+
+let closePopup = document.querySelector('.popup__button-close');
+
+let formInput = document.querySelector('.popup__input');
+ 
+let nameInput = document.querySelector('.popup__item_el_name');
+
+let jobInput = document.querySelector('.popup__item_el_job');
+
+let userInfo = document.querySelector('profile__user-information')
+
+let userName = document.querySelector('.profile__user-name');
+
+let userJob = document.querySelector('.profile__user-subname');
+
+function popupOpen () {
+    nameInput.value = userName.textContent;
+    jobInput.value = userJob.textContent;
+
+    popup.classList.add('popup_opened')
+}
+
+function popupClose ()  {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    userName.textContent = nameInput.value;
+    userJob.textContent = jobInput.value;
+
+}
+
+openPopup.addEventListener('click', popupOpen);
+
+closePopup.addEventListener('click', popupClose);
+
+formInput.addEventListener('submit', formSubmitHandler); 
+
+
