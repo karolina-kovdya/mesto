5b14430129b324ca63af99110051cf07d6746b7f
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2022-08-22 16:25:51 +0300

    добавить новый popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
 let popup = document.querySelector('.popup');
 
+let popupAddCard = document.querySelector('.popup_add-card');
+
 let form = document.querySelector('.popup__form')
 
 let openPopup = document.querySelector('.profile__button_type_edit');
+
+let AddPopup = document.querySelector('.profile__button_type_add');
 
 let closePopup = document.querySelector('.popup__button-close');
 
@@ -18,15 +22,32 @@ let userName = document.querySelector('.profile__user-name');
 
 let userJob = document.querySelector('.profile__user-subname');
 
+let titleInput = formInput.querySelector('.popup__item_el_title');
+
+let fotoInput = formInput.querySelector('.popup__item_el_foto');
+
+let placeTitle = document.querySelector('.place__title');
+
+let placeFoto = document.querySelector('.place__image');
+
 function popupOpen () {
     nameInput.value = userName.textContent;
     jobInput.value = userJob.textContent;
 
-    popup.classList.add('popup_opened')
+    popup.classList.add('popup_opened');
+}
+
+function popupOpenAdd () {
+
+    popupAddCard.classList.add('popup_opened');
 }
 
 function popupClose ()  {
     popup.classList.remove('popup_opened');
+}
+
+function popupAddClose () {
+    popupAddCard.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -40,7 +61,11 @@ function formSubmitHandler (evt) {
 
 openPopup.addEventListener('click', popupOpen);
 
+AddPopup.addEventListener('click', popupOpenAdd);
+
 closePopup.addEventListener('click', popupClose);
+
+closePopup.addEventListener('click', popupAddClose);
 
 form.addEventListener('submit', formSubmitHandler); 
 
